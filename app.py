c39d3e6595659ee7eff5a36f21bc4d0a4dac1c8a
Christian Hasbany <christian_hasbani@etu.u-bourgogne.fr>
2023-10-10 13:45:31 +0200

    Added some modifications - working on V2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from datetime import datetime
 import redis
 import json
+import hashlib
 
 app = Flask(__name__)
 
@@ -12,8 +13,9 @@ r1 = redis.Redis(host='localhost', port=6379, db=1,decode_responses=True)
 time = datetime(2023,1,1).timestamp() 
 
 # Tuple initialization
-add1 = ("Antoine","Christian",time,10)
-add2 = ("Antoine","Christian",time,200)
+# Structure of a transation (P1,P2,t,a,h)
+add1 = ("Antoine","Christian",time,10,None)
+add2 = ("Antoine","Christian",time,200,None)
 
 # Convert the tuples to JSON strings
 add1_json = json.dumps(add1)
@@ -24,7 +26,7 @@ r.set("add1", add1_json)
 r.set("add2", add2_json)
 
 # Dictionary initialization
-transaction = [add1,add2]
+transations = [add1,add2]
 
 
 # Function to return all of the dictionary
@@ -33,8 +35,8 @@ def getList():
     if request.method == 'GET':
 
         # Sort the dictionary by date
-        transaction.sort()
-        return str(transaction)
+        transations.sort()
+        return str(transations)
 
 
 # Function to return all of the dictionary of a person
@@ -46,10 +48,10 @@ def getListPerson(Person):
         result = ""
 
         # Sort the dictionary by date
-        transaction.sort()
+        transations.sort()
 
         # Loop through the dictionary
-        for i in transaction:
+        for i in transations:
 
             # Check if the person in the dictonary is egal to the person in the GET
             if i[0] == Person:
@@ -61,7 +63,7 @@ def getListPerson(Person):
         if(result == ""):
             return "Person not found"
 
-        # If the person is in the dictionary return the solde of transaction
+        # If the person is in the dictionary return the solde of transations
         else:
             return result
 
@@ -74,7 +76,7 @@ def getSolde(Person):
     solde = 0
 
     # Loop through the dictionary
-    for i in transaction:
+    for i in transations:
 
         # Check if the person in the dictonary is egal to the person in the GET
         if i[0] == Person:
@@ -101,14 +103,38 @@ def addElement():
         # Get the current date in second since 2023
         time = datetime(2023,1,1).timestamp()
 
+        # Initialize the tupple
+        add = (person1,person2,time,solde,None)
+
+        # Compute the hash and update the tuple
+        add = (*add[:-1],compute_hash(add))
+
         # Add the element in a tuple
-        add = (person1,person2,time,solde)
-        r.save(add)
+        add_str = json.dumps(add)
+        key = "add" + str((len(transations) + 1))
+        r.set(key,add_str)
     
         # Add the tuple in the dictionary
-        transaction.append(add)
+        transations.append(add)
 
-    
 
         return "You have successfully added a new element:" + str(add)
     return "You have not added a new element"
+
+# Endpoint to check if all the transations hash is correct
+@app.route("/check_integrity", methods=['GET'])
+def checkIntegrity():
+    for i,transaction_tuple in enumerate(transations):
+        recalculated_hash = compute_hash(transaction_tuple[:-1])
+        if recalculated_hash != transaction_tuple[:-1]:
+            return f"Integrity check failed for transation {i+1}"
+    return "Integrity check passed for all transations"
+# THIS IS STILL NOT WORKING !!!!
+
+
+# Method to compute the hash
+def compute_hash(transation_tuple):
+    transation = transation_tuple[:-1] # Remove the last element from the transtation tuple which contains the hash 
+    data_str = json.dumps(transation, sort_keys = True) # Convert to JSON object
+    return hashlib.sha256(data_str.encode()).hexdigest() # Use SHA-256 function
+
